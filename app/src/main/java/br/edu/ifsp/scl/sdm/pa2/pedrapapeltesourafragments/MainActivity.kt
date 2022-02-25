f149cbc22ed2a0bd308a5cd255693114a4f37c23
Roger Peratello <roger.peratello@hotmail.com>
2022-02-25 02:27:43 -0300

    Fragment design started

--- app/src/main/java/br/edu/ifsp/scl/sdm/pa2/pedrapapeltesourafragments/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sdm/pa2/pedrapapeltesourafragments/MainActivity.kt
@@ -2,10 +2,18 @@ package br.edu.ifsp.scl.sdm.pa2.pedrapapeltesourafragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.edu.ifsp.scl.sdm.pa2.pedrapapeltesourafragments.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val activityMainBinding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(activityMainBinding.root)
+
+        supportFragmentManager?.beginTransaction().add(R.id.mainFcv, MainFragment(), "MainFragment").commit()
+
     }
 }
